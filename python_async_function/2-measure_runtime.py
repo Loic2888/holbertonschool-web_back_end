37d8b520cf08e5a2d9e9f5bf4275df89fd531c41
Loic2888 <12188@holbertonstudents.com>
2026-04-08 16:14:33 +0200

    feat: add the task 2 of the project 2 of this week

---
+++ python_async_function/2-measure_runtime.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Measure the runtime of wait_n"""
+
+import time
+import asyncio
+from typing import Union
+
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    """Measure the average runtime of wait_n(n, max_delay)."""
+    start = time.time()
+    asyncio.run(wait_n(n, max_delay))
+    end = time.time()
+    total_time = end - start
+    return total_time / n
