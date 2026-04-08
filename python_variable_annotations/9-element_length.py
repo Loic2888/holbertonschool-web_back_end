522e3cdedf81fc0e102f90404c1f59cb3561d2b8
Loic2888 <12188@holbertonstudents.com>
2026-04-08 15:42:10 +0200

    fix: typping error

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
-def element_length(lst: list) -> list:
+
+from typing import Iterable, List, Sequence, Tuple
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """Returns a list of the lengths of the elements in a list."""
-    return [len(i) for i in lst]
+    return [(i, len(i)) for i in lst]
