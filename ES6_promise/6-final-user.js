fbab6f5203d3ec33e283fd060d4802ad6b2bf7d9
Loic2888 <12188@holbertonstudents.com>
2026-05-08 18:03:54 +0200

    fix: fail, filename -> fileName

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,7 +2,7 @@ import signUpUser from './4-user-promise.js';
 import uploadPhoto from './5-photo-reject.js';
 
 
-export default function handleProfileSignup(firstName=String, lastName=String, filename=String) {
+export default function handleProfileSignup(firstName=String, lastName=String, fileName=String) {
   return Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName)
