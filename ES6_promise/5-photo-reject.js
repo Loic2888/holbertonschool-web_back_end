2712d9d9c1096c78524944af5f5596fb7025b1d6
Loic2888 <12188@holbertonstudents.com>
2026-05-08 17:45:54 +0200

    feat: add the task 5 of the second project ES6 of this week

---
+++ ES6_promise/5-photo-reject.js
@@ -0,0 +1,9 @@
+export default function uploadPhoto(filename) {
+  return new Promise((resolve, reject) => {
+    if (filename) {
+      resolve(filename);
+    } else {
+      reject(new Error('$fileName cannot be processed'));
+    }
+  });
+}
