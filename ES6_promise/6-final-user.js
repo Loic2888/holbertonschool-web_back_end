8c313bcf21d5804be08f13ab38066a8c31b4dfcf
Loic2888 <12188@holbertonstudents.com>
2026-05-08 18:07:20 +0200

    fix: fail tchecker, try again

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,10 +1,14 @@
 import signUpUser from './4-user-promise.js';
 import uploadPhoto from './5-photo-reject.js';
 
-
 export default function handleProfileSignup(firstName=String, lastName=String, fileName=String) {
   return Promise.allSettled([
     signUpUser(firstName, lastName),
-    uploadPhoto(fileName)
-  ]);
+    uploadPhoto(fileName),
+  ]).then((results) =>
+    results.map((item) => ({
+      status: item.status,
+      value: item.status === 'fulfilled' ? item.value : item.reason,
+    }))
+  );
 }
