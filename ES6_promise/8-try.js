76964c707a8a7500408b092cb6fcedfab6ea10ed
Loic2888 <12188@holbertonstudents.com>
2026-05-08 18:46:48 +0200

    fix: fail checker, try again

--- ES6_promise/8-try.js
+++ ES6_promise/8-try.js
@@ -1,11 +1,6 @@
-import { interpolate } from "eslint/lib/linter";
-
 export default function divideFunction(numerator=Number, denominator=Number) {
-  return new Promise((resolve, reject) => {
-    if (denominator === 0) {
-      reject(new Error('cannot divide by 0'));
-    } else {
-      resolve(numerator / denominator);
-    }
-  });
+  if (denominator === 0) {
+    throw new Error('cannot divide by 0');
+  }
+  return numerator / denominator;
 }
