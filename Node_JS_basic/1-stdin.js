419c3e53f46e3e98d774767dfe907823d6530d3f
Loic2888 <12188@holbertonstudents.com>
2026-05-11 21:45:31 +0200

    fix: fail again, try another thing, i think this time, that will work

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,15 +1,10 @@
 process.stdin.setEncoding('utf8');
- 
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
- 
+console.log('Welcome to Holberton School, what is your name?');
 process.stdin.on('data', (data) => {
   const input = data.trim();
   process.stdout.write(`Your name is: ${input}\n`);
-  process.stdin.destroy();
+  process.stdin.pause();
 });
- 
-process.stdin.on('close', () => {
-  if (!process.stdin.isTTY) {
-    process.stdout.write('This important software is now closing\n');
-  }
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
