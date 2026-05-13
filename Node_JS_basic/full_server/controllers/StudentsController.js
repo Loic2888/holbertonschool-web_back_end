f8797873e86b9795ffba901e3b6e0dab54ba779b
Loic2888 <12188@holbertonstudents.com>
2026-05-13 18:53:39 +0200

    fix: try something else

--- Node_JS_basic/full_server/controllers/StudentsController.js
+++ Node_JS_basic/full_server/controllers/StudentsController.js
@@ -2,41 +2,27 @@ import readDatabase from '../utils';
 
 export default class StudentsController {
   static getAllStudents(request, response) {
-    const databaseFile = process.argv[2];
-
-    readDatabase(databaseFile)
+    readDatabase(process.argv[2])
       .then((students) => {
-        const sortedFields = Object.keys(students)
-          .sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()));
-
-        const lines = [
-          'This is the list of our students',
-          ...sortedFields.map((field) => `Number of students in ${field}: ${students[field].length}. List: ${students[field].join(', ')}`),
-        ];
-
-        response.status(200).send(lines.join('\n'));
+        let result = 'This is the list of our students';
+        for (const [key] of Object.entries(students).sort()) {
+          result += `\nNumber of students in ${key}: ${students[key].length}. List: ${students[key].join(', ')}`;
+        }
+        response.status(200).send(result);
       })
-      .catch(() => {
-        response.status(500).send('Cannot load the database');
-      });
+      .catch(() => response.status(500).send('Cannot load the database'));
   }
 
   static getAllStudentsByMajor(request, response) {
-    const databaseFile = process.argv[2];
-    const { major } = request.params;
-
-    if (major !== 'CS' && major !== 'SWE') {
-      response.status(500).send('Major parameter must be CS or SWE');
-      return;
-    }
-
-    readDatabase(databaseFile)
+    readDatabase(process.argv[2])
       .then((students) => {
-        const list = students[major] || [];
-        response.status(200).send(`List: ${list.join(', ')}`);
+        if (!Object.keys(students).includes(request.params.major)) {
+          response.status(500).send('Major parameter must be CS or SWE');
+          return;
+        }
+        const list = students[request.params.major].join(', ');
+        response.status(200).send(`List: ${list}`);
       })
-      .catch(() => {
-        response.status(500).send('Cannot load the database');
-      });
+      .catch(() => response.status(500).send('Cannot load the database'));
   }
 }
